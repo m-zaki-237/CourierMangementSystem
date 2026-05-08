34484681d67068eeb9bd836f239cf7e18a557af5
m-zaki-237 <muhammadzakriaawan237@gmail.com>
2026-05-08 20:53:19 +0500

    gui

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,95 +1,12 @@
-package main;
-
-import models.*;
-import services.*;
+import gui.LoginFrame;
+import services.UserService;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ParcelService parcelService = new ParcelService();
         UserService userService = new UserService();
 
-        Customer customer = new Customer(
-                1,
-                "Zaki",
-                "zaki@gmail.com",
-                "123"
-        );
-
-        Admin admin = new Admin(
-                2,
-                "Admin",
-                "admin@gmail.com",
-                "admin"
-        );
-
-        DeliveryAgent agent = new DeliveryAgent(
-                3,
-                "Ali Agent",
-                "agent@gmail.com",
-                "agent"
-        );
-
-        userService.addUser(customer);
-        userService.addUser(admin);
-        userService.addUser(agent);
-
-        User loggedCustomer = userService.login("zaki@gmail.com", "123");
-        User loggedAdmin = userService.login("admin@gmail.com", "admin");
-        User loggedAgent = userService.login("agent@gmail.com", "agent");
-
-        System.out.println("\n===== CUSTOMER REQUEST PARCEL =====");
-
-        Address addr = new Address("Street 1", "Lahore");
-
-        Parcel parcel = parcelService.requestParcel(
-                loggedCustomer,
-                "Ali",
-                "ali@gmail.com",
-                addr,
-                2.5
-        );
-        
-        if (parcel == null) return;
-
-        System.out.println("\n===== ADMIN APPROVES PARCEL =====");
-
-        parcelService.approveParcel(loggedAdmin, parcel);
-
-        System.out.println("\n===== ADMIN ASSIGNS AGENT =====");
-
-        parcelService.assignAgent(loggedAdmin, parcel, agent);
-
-        System.out.println("\n===== AGENT UPDATES DELIVERY =====");
-
-        parcelService.updateDeliveryStatus(
-                loggedAgent,
-                parcel,
-                ParcelStatus.SHIPPED,
-                "Lahore Hub"
-        );
-
-        parcelService.updateDeliveryStatus(
-                loggedAgent,
-                parcel,
-                ParcelStatus.IN_TRANSIT,
-                "On the way to destination"
-        );
-
-        parcelService.updateDeliveryStatus(
-                loggedAgent,
-                parcel,
-                ParcelStatus.DELIVERED,
-                "Delivered at customer address"
-        );
-
-        System.out.println("\n===== TRACK PARCEL =====");
-
-        parcelService.trackParcel(parcel.getTrackingId());
-
-        System.out.println("\n===== ADMIN VIEW ALL PARCELS =====");
-
-        parcelService.showAllParcels(loggedAdmin);
+        new LoginFrame(userService);
     }
 }
